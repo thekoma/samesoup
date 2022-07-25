cfd979f4139c5479f95b373e59e1d7d3d6c62156
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-07-25 18:27:34 +0200

    True!

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -2,5 +2,5 @@ resource "google_storage_bucket" "utils" {
   name                        = "utils-${random_string.suffix.result}"
   project                     = module.project-factory.project_id
   location                    = var.location
-  uniform_bucket_level_access = false
+  uniform_bucket_level_access = true
 }
