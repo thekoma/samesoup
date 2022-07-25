471aeef58b997f7d400ce353e24c5a402de8c151
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-07-25 18:21:28 +0200

    random names

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "utils" {
-  name          = "utils"
+  name          = "utils-${random_string.suffix.result}"
   project       = module.project-factory.project_id
   location      = "US"
 }
