42eeaebecf50f5bcc36bffcd943bd714f2c60b8f
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-08-12 15:12:37 +0200

    cleanup

--- terraform/modules/replatform/repo.tf
+++ terraform/modules/replatform/repo.tf
@@ -8,7 +8,7 @@ resource "null_resource" "align_repo_from_template" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-      command = "ansible-playbook -v ${path.module}/ansible/playbook.yaml"
+      command = "ansible-playbook -vv ${path.module}/ansible/playbook.yaml"
       environment = {
         TEMPLATE_GIT = "${var.template_git}"
         TEMPLATE_PATH = "${var.template_path}"
