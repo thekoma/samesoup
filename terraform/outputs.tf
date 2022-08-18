097f89f5aaaacfdd5b644d640cdb364de4db8dfd
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-08-18 18:49:20 +0200

    added Output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,27 @@
-# output "kanban_password" {
-#   value = nonsensitive(random_password.kanban_password.result)
-# }
+data "google_dns_managed_zone" "dns_zone" {
+  name    = var.dns_zone
+  project = var.dns_project_id
+}
+locals {
+  dns_basename = "${var.dns_prefix}.${trimsuffix(data.google_dns_managed_zone.dns_zone.dns_name, ".")}"
+}
 
-# output "rehost_url" {
-#   value = "https://rehost.${local.dns_basename}"
-# }
+output "explanation" {
+  value = <<EOT
+  ##### That's the SameSoup it just changes the plate. #####
 
-# output "rehost_mig_url" {
-#   value = "https://rehost-mig.${local.dns_basename}"
-# }
+  We have created a PHP application (kanban) installed in 4 different ways.
+  Each installation has the same database and shares the same configuration (and so the same storage backend).
+  This means that you can actually connect to any of these endpoint and it will result in the same site:
+  Rehost:     https://rehost.${local.dns_basename}      <- This is a lift and shift
+  ReHost-Mig: https://rehost-mig.${local.dns_basename}  <- A more complex lift and shift where the backend will autoscale on multiple hosts.
+  Replatform: https://replatform.${local.dns_basename}  <- The site is now converted into a container and runs into GKE
+  Refactor:   https://replatform.${local.dns_basename}  <- The end of the curve, the application is now only a container as a service in Cloud Run
 
-# output "replatform_url {
-#   value = "https://replatform.${local.dns_basename}"
-# }
+  The credentials are:
+  user: admin
+  password: admin
 
-# output "rehost_mig_domain" {
-#   value = local.rehost_mig_domain
-# }
+  Feel free to break it.
+  EOT
+}
